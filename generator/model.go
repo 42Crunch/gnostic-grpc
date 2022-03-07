964c542a319a2e3cdefbed2b3b434c17b915d304
Karim Benhammou <karim.benhammou@42crunch.com>
2022-03-07 18:20:19 +0100

    escape caret and new line when adding a pattern

--- generator/model.go
+++ generator/model.go
@@ -35,7 +35,9 @@ func Float64(i float64) *float64 {
 func (f *Field) toTags() map[string]string {
 	tags := make(map[string]string)
 	if f.ParamPattern != nil {
-		pattern := strings.Replace(*f.ParamPattern, `\`, `\\`, -1)
+		pattern := strings.Replace(*f.ParamPattern, "\n", "\\n", -1)
+		pattern = strings.Replace(pattern, "\r", "\\r", -1)
+		pattern = strings.Replace(pattern, `\w`, "\\w", -1)
 		tags["pattern"] = pattern
 	}
 	if f.ParamFormat != nil {
